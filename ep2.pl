e6aa63d9f1df613057703dbaa9be9c58141fe801
Ricardo e Danilo <ricardo.morita@gmail.com>
2013-11-29 18:41:38 -0200

    versao quase pronta, ainda sem CNF

--- ep2.pl
+++ ep2.pl
@@ -1,11 +1,21 @@
 #!/usr/bin/perl
 =begin comment
-NOSSO CABECALHO
+    *** EP 2 ***
+    
+    Nomes:	
+    	Danilo Novais (NUSP 7990759)
+    	Ricardo Mikio Morita (NUSP 5412562)
+    	
+    Matéria:
+    	Métodos Formais
+
+    Referências:
+    	
 =end comment
 =cut
 
-#use strict;
-#use warnings;
+use strict;
+use warnings;
 
 ########################Declaracao de Variaveis Globais########################
 
@@ -19,24 +29,66 @@ my @predicados;
 
 ########################Declaracao de Funcoes##################################
 
+sub substituiValores2
+{
+	my $predAtual = shift;
+    my @restricoes = split(/,/, shift);
+    
+    my $pertenceDominio = 1;
+		
+	#Substitui as variaveis nas clausulas pelos indices das mesmas
+	foreach my $variavel (@variaveis)
+	{
+		$predAtual =~ s/$variavel/$variaveis{$variavel}->[2]/gx;
+		
+		foreach my $restAtual (@restricoes)
+		{
+			$restAtual =~ s/$variavel/$variaveis{$variavel}->[2]/gx;
+		}
+	}
+			
+	#Prepara as restricoes de dominio para tratamento condicional
+	foreach my $restAtual(@restricoes) 
+	{
+		#Retira os espaços em branco
+		$restAtual =~ s/\s//gx;
+		
+		#Substitui "=" por "==" caso nao seja "!=", "<=" ou ">="
+		if ($restAtual !~ m/(!=|<=|>=)/gx)
+		{
+			$restAtual =~ s/=/==/x;
+		}
+		
+		if (!(eval $restAtual))
+		{
+			$pertenceDominio = 0;
+		}
+	}
+	
+	if ($pertenceDominio)
+	{
+		print $predAtual . "\n";
+	}
+}
+
 sub substituiValores
 {
 	foreach my $predicado (@predicados)
 	{
 		my $predAtual = $predicado->[0];
-		foreach my $variavel (@variaveis)
-		{
-			while ($predAtual =~ s/$variavel/$variaveis{$variavel}->[2]/x)
-			{
-				my $a = "O rei bebe cafe";
-			}	
-		}
-		print $predAtual . "\n";
+		my $restricoes = $predicado->[1];
+		
+		#my @restricoes = split(/,/, $predicado->[1]);
+		
+		atualizaVariaveis($predAtual,$restricoes,@variaveis);				
 	}
 }
 
 sub atualizaVariaveis
 {	
+	my $predicado = shift;
+	my $restricao = shift;
+	
 	unless (@_ == 0)
 	{
 		my $atual = shift @_;
@@ -44,12 +96,13 @@ sub atualizaVariaveis
 		for (my $i = $variaveis{$atual}->[0]; $i <= $variaveis{$atual}->[1]; $i++)
 		{
 			$variaveis{$atual}->[2] = $i;
-			atualizaVariaveis(@_);
+			atualizaVariaveis($predicado,$restricao,@_);
 			if (@_ == 0)
 			{
-				substituiValores();
+				substituiValores2($predicado,$restricao);
 			}
 		}
+		$variaveis{$atual}->[2] = $variaveis{$atual}->[0];
 	}
 }
 
@@ -89,7 +142,7 @@ while (<>) {
 		}
 		else
 		{
-		 	$predicados[$i-1][1] = "";
+		 	$predicados[$i-1][1] = "1";
 		}
 	}
 	
@@ -114,23 +167,17 @@ while (<>) {
 # por uma string de clausulas([0]) e uma string de restricoes([1]).
 
 @variaveis = keys %variaveis;
-print @variaveis;
+print "Variaveis: " . @variaveis;
 
 print "\n";
 #print keys %variaveis;
-print $predicados[0][0] . " e aqui as restricoes: " . $predicados[0][1] . "\n";
+print "Clausulas: \"" . $predicados[0][0] . "\" , com as restricoes: \"" . $predicados[0][1] . "\"\n\n";
 #print $predicados[1][0] . " e aqui as restricoes: " . $predicados[1][1] . "\n";
 #print $predicados[2][0] . " e aqui as restricoes: " . $predicados[2][1] . "\n";
 
-
-
-
-
-
-atualizaVariaveis (@variaveis);
-
+#atualizaVariaveis (@variaveis);
+substituiValores();
+#print "\n". $restricoes[0] . $restricoes[1];
 
 
 close(FILEOUT);
-
-
